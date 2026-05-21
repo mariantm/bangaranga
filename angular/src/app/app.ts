4c13e63fbae345dd428dd2503b03bcb95c8893e9
Marian Marinov <marian.marinov@sirma.com>
2026-05-21 18:34:57 +0300

    Service has been consumed

--- angular/src/app/app.ts
+++ angular/src/app/app.ts
@@ -1,6 +1,9 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { ConsumeSpringSecurityService } from './ConsumeSpringSecurityService';
+
+
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
@@ -9,4 +12,20 @@ import { RouterOutlet } from '@angular/router';
 })
 export class App {
   protected readonly title = signal('bangaranga');
+
+
+  clicked() {
+      alert('asdasd')
+  }
+
+  result = signal('');
+  constructor(private service: ConsumeSpringSecurityService) { }
+  ngOnInit() {
+      this.service.getHome().subscribe({
+        next: (data) => this.result.set(data),
+        error: (err) => console.error('Error:', err)
+      });
+  }
+
+
 }
